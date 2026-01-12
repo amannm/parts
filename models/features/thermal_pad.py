82a762d8f51d5734b360415c55b8938bf844b8ca
Amann Malik <amannmalik@gmail.com>
2026-01-11 19:28:23 -0600

    refactor

--- models/features/thermal_pad.py
+++ models/features/thermal_pad.py
@@ -6,7 +6,7 @@ from typing import Protocol
 import cadquery as cq
 from cadquery import selectors
 
-from features.pin import LeadLayout, positions
+from models.features.pin import LeadLayout, positions
 
 
 @dataclass(frozen=True)
